052157f7abb3d0dcd4a2e475aaddeaeac2a3bbd1
Daniel Leech <dan.t.leech@gmail.com>
2019-04-21 08:46:14 +0100

    Resetting again

--- lib/Model/Unit/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Maestro\Model\Unit;
-
-use Symfony\Component\OptionsResolver\OptionsResolver;
-
-class Config extends OptionsResolver
-{
-}
--- lib/Model/Unit/Registry.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Maestro\Model\Unit;
-
-interface Registry
-{
-    public function get(string $unit): Unit;
-}

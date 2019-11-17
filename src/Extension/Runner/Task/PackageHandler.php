f8c936c102cee69f5b252be8156e957e3f0c4660
Daniel Leech <dan.t.leech@gmail.com>
2019-11-17 17:16:39 +0000

    Do not require environment in package handler

--- src/Extension/Runner/Task/PackageHandler.php
+++ src/Extension/Runner/Task/PackageHandler.php
@@ -10,16 +10,11 @@ use Maestro\Library\Support\Package\Package;
 class PackageHandler
 {
     public function __invoke(
-        PackageTask $task,
-        Environment $environment
+        PackageTask $task
     ): Promise {
-        return \Amp\call(function () use ($task, $environment) {
+        return \Amp\call(function () use ($task) {
             $name = $task->name();
-            $environment = $environment->spawnMerged([
-                'PACKAGE_NAME' => $name
-            ]);
             return new Success([
-                $environment,
                 new Package(
                     $name,
                     $task->version()
